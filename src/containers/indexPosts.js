4bb4e1206c10cb51f55e1cef05bc3c6b858ae8b0
divyakalidindi <divyakalidindi@divyas-air.wireless.dartmouth.edu>
2016-08-02 17:43:46 -0400

    updates to home

--- src/containers/indexPosts.js
+++ src/containers/indexPosts.js
@@ -1,1 +1,30 @@
+import React, { Component } from 'react';
+import { fetchPosts } from '../actions';
+import { connect } from 'react-redux';
 
+class Home extends Component {
+
+
+  componentWillMount() {
+    this.props.fetchPosts();
+  }
+
+  render () {
+      if ( this.props.posts == null ) {
+        console.log('null');
+        <h1>There are no posts!</h1>
+      } else {
+          return this.props.posts.map((post) => {
+            // return Link?
+            console.log('return a link?');
+          });
+      }
+    }
+
+const mapStateToProps = (state) => {
+
+  posts: state.posts.all
+
+}};
+
+export default connect(mapStateToProps, { fetchPosts })(Home);
